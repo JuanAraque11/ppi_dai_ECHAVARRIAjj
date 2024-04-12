c127d5a53845687b22f08507797d1620886c56ff
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2024-04-12 16:48:15 -0500

    Update info.py

--- Un_mundo_en_tu_plato/info.py
+++ Un_mundo_en_tu_plato/info.py
@@ -9,9 +9,8 @@ def info():
 
     # Enlaces a secciones específicas
     st.write("Para obtener más detalles, visita las siguientes secciones:")
-    st.write("- Acerca de Nosotros")
-    st.write("- Política de Privacidad")
-    st.write("- Contacto")
+    st.markdown("- [Política de Privacidad](https://tuempresa.com/politica-de-privacidad)")
+    st.markdown("- [Contacto](www.linkedin.com/in/juan-jose-echavarria-araque-a92286296)")
 
     # Pie de página
     st.write("© 2024 Mi Empresa. Todos los derechos reservados.")
