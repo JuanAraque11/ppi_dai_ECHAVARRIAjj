a52d140b069660ae0f8ac05183d04dcfb354c7cb
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2024-04-13 14:21:53 -0500

    Update secun.py

---
+++ Un_mundo_en_tu_plato/Pagina_secundaria/secun.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+# Título de la página
+st.title("Contenido")
+
+# Menú de opciones desplegables
+opcion = st.selectbox("Selecciona una opción:", ["Opción 1", "Opción 2", "Opción 3"])
+
+# Mostrar contenido según la opción seleccionada
+if opcion == "Opción 1":
+    st.write("Este es el contenido de la Opción 1.")
+elif opcion == "Opción 2":
+    st.write("Este es el contenido de la Opción 2.")
+elif opcion == "Opción 3":
+    st.write("Este es el contenido de la Opción 3.")
