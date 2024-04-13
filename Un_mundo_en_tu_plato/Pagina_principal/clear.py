bb318f71cb9bb453277699663d1126e6ca6f54a4
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2024-04-13 14:06:43 -0500

    Update clear.py

--- Un_mundo_en_tu_plato/Pagina_principal/clear.py
+++ Un_mundo_en_tu_plato/Pagina_principal/clear.py
@@ -2,11 +2,3 @@ import streamlit as st
 
 def clear_page():
     st.empty()
-
-button = st.button("Borrar página")
-
-if button:
-    clear_page()
-
-# Agregar nuevo contenido después de borrar la página
-new_content = st.write("Este es el nuevo contenido")
