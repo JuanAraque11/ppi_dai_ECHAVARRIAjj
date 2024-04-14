d702193bbb28702a52a23ece59bbe08fcf1af0b8
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2024-04-14 13:56:03 -0500

    cambios

--- Un_mundo_en_tu_plato/Pagina_principal/info.py
+++ Un_mundo_en_tu_plato/Pagina_principal/info.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-def info():
+def info1():
     # Título del apartado
     st.title("Información Importante")
 
@@ -14,3 +14,21 @@ def info():
 
     # Pie de página
     st.write("© 2024 Mi Empresa. Todos los derechos reservados.")
+
+def info2():
+    # Título y autor
+    st.title("Un mundo en tu plato")
+    st.write('''¡Emprende un viaje culinario alrededor del mundo sin salir de casa!
+    Descubre recetas auténticas de cada rincón del planeta, desde la vibrante cocina mexicana hasta los exóticos sabores tailandeses.
+
+    Explora una amplia variedad de platos, desde clásicos familiares hasta innovadoras creaciones de chefs galardonados.
+
+    Aprende a preparar platos deliciosos con instrucciones detalladas paso a paso y videos explicativos.
+
+    Conoce los ingredientes de cada receta, su origen y sus propiedades nutricionales.
+
+    Personaliza tu experiencia con filtros avanzados que te permiten buscar recetas por región, tipo de cocina, ingredientes y preferencias dietéticas.
+
+    Guarda tus recetas favoritas para acceder a ellas fácilmente más tarde.
+
+    Comparte tus descubrimientos culinarios con amigos y familiares en las redes sociales. ''')
--- Un_mundo_en_tu_plato/Pagina_principal/principal.py
+++ Un_mundo_en_tu_plato/Pagina_principal/principal.py
@@ -3,23 +3,6 @@ import info
 import login
 import register
 
-# Título y autor
-st.title("Un mundo en tu plato")
-st.write('''¡Emprende un viaje culinario alrededor del mundo sin salir de casa!
-Descubre recetas auténticas de cada rincón del planeta, desde la vibrante cocina mexicana hasta los exóticos sabores tailandeses.
-
-Explora una amplia variedad de platos, desde clásicos familiares hasta innovadoras creaciones de chefs galardonados.
-
-Aprende a preparar platos deliciosos con instrucciones detalladas paso a paso y videos explicativos.
-
-Conoce los ingredientes de cada receta, su origen y sus propiedades nutricionales.
-
-Personaliza tu experiencia con filtros avanzados que te permiten buscar recetas por región, tipo de cocina, ingredientes y preferencias dietéticas.
-
-Guarda tus recetas favoritas para acceder a ellas fácilmente más tarde.
-
-Comparte tus descubrimientos culinarios con amigos y familiares en las redes sociales. ''')
-
 # Menú desplegable para elegir entre iniciar sesión y registrar usuario
 opcion = st.sidebar.selectbox("Menú:", ["Inicio", "Iniciar sesión", "Registrarse"])
 # Función principal
@@ -35,9 +18,9 @@ def main():
         if st.button("Ver lista de usuarios"):
             register.show_registered_users()
     elif opcion == "Inicio":
-        pass
+        info.info2()
 
-    info.info()
+    info.info1()
 
 
 if __name__ == "__main__":
