d4b1b5ce8dab7a50c35a51fd7470352a85cbc1dd
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2024-04-14 13:40:57 -0500

    Update secun.py

--- Un_mundo_en_tu_plato/Pagina_secundaria/secun.py
+++ Un_mundo_en_tu_plato/Pagina_secundaria/secun.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from graficos import mostrar_opcion_1
+import graficos
 
 # Título de la página
 st.title("Contenido")
@@ -10,7 +10,7 @@ opcion = st.selectbox("Selecciona una opción:", ["Opción 1", "Opción 2", "Opc
 # Mostrar contenido según la opción seleccionada
 if opcion == "Opción 1":
     st.write("Este es el contenido de la Opción 1.")
-    mostrar_opcion_1()
+    graficos.mostrar_opcion_1()
 elif opcion == "Opción 2":
     st.write("Este es el contenido de la Opción 2.")
 elif opcion == "Opción 3":
