59559deb4408d16d2bc954f2c9d381acb7b5fbf9
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2024-04-14 20:51:38 -0500

    Update secun.py

--- Un_mundo_en_tu_plato/Pagina_secundaria/secun.py
+++ Un_mundo_en_tu_plato/Pagina_secundaria/secun.py
@@ -1,6 +1,28 @@
 import streamlit as st
 # import graficos
 
+# Botón para mostrar las opciones del usuario
+if "user" not in st.session_state:
+    st.session_state.user = False  # Establecer el estado inicial del botón
+
+if st.button("Usuario"):
+    st.session_state.user = not st.session_state.user  # Alternar el estado del botón
+
+if st.session_state.user:
+    st.write("Aquí puedes escribir tu user.")
+
+if st.button("Usuario"):
+    # Desplegar menú de opciones
+    option = st.selectbox("Selecciona una opción:", ["Actualizar Contraseña", "Ver Perfil"])
+    
+    # Actuar según la opción seleccionada
+    if option == "Actualizar Contraseña":
+        st.write("Opción seleccionada: Actualizar Contraseña")
+        # Aquí puedes colocar el código para actualizar la contraseña
+    elif option == "Ver Perfil":
+        st.write("Opción seleccionada: Ver Perfil")
+        # Aquí puedes colocar el código para ver el perfil del usuario
+
 # Título de la página
 st.title("Contenido")
 
@@ -15,15 +37,3 @@ elif opcion == "Opción 2":
     st.write("Este es el contenido de la Opción 2.")
 elif opcion == "Opción 3":
     st.write("Este es el contenido de la Opción 3.")
-
-if st.button("Usuario"):
-    # Desplegar menú de opciones
-    option = st.selectbox("Selecciona una opción:", ["Actualizar Contraseña", "Ver Perfil"])
-    
-    # Actuar según la opción seleccionada
-    if option == "Actualizar Contraseña":
-        st.write("Opción seleccionada: Actualizar Contraseña")
-        # Aquí puedes colocar el código para actualizar la contraseña
-    elif option == "Ver Perfil":
-        st.write("Opción seleccionada: Ver Perfil")
-        # Aquí puedes colocar el código para ver el perfil del usuario
