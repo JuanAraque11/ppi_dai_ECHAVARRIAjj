060902adf49833fda5e22c519315557f4b517d29
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2024-04-14 22:11:24 -0500

    Update funciones.py

---
+++ Un_mundo_en_tu_plato/Pagina_principal/funciones.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def mostrar_inicio():
+    st.write("Bienvenido al Inicio")
+    st.write("Aquí encontrarás información sobre las funciones de la página:")
+    st.write("- Guardar Recetas: Permite a los usuarios guardar nuevas recetas en la base de datos.")
+    st.write("- Consultar Información de los Platos: Proporciona información detallada sobre los platos disponibles.")
+    st.write("- Ver Distribuciones Estadísticas: Muestra distribuciones estadísticas sobre los platos.")
