d74242267e564aaf1adc40c4a60b1cbed8993e0a
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2024-04-14 20:59:04 -0500

    Update secun.py

--- Un_mundo_en_tu_plato/Pagina_secundaria/secun.py
+++ Un_mundo_en_tu_plato/Pagina_secundaria/secun.py
@@ -7,7 +7,11 @@ if st.button("Usuario"):
     # Actuar según la opción seleccionada
     if option == "Actualizar Contraseña":
         st.write("Opción seleccionada: Actualizar Contraseña")
-        # Aquí puedes colocar el código para actualizar la contraseña
+        contraseña_actual = st.text_input("Contraseña actual:", type="password")
+        nueva_contraseña = st.text_input("Nueva contraseña:", type="password")
+        if st.button("Actualizar"):
+            # Aquí puedes colocar el código para verificar la contraseña actual y actualizarla
+            st.success("¡Contraseña actualizada con éxito!")
     elif option == "Ver Perfil":
         st.write("Opción seleccionada: Ver Perfil")
         # Aquí puedes colocar el código para ver el perfil del usuario
