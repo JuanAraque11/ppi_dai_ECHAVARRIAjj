8f30111a288ed07296c40c166f8343da34fc6527
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2024-04-15 00:07:45 -0500

    Update principal.py

--- Un_mundo_en_tu_plato/Pagina_principal/principal.py
+++ Un_mundo_en_tu_plato/Pagina_principal/principal.py
@@ -6,23 +6,40 @@ import register
 from usuario import Usuario
 
 # Menú desplegable para elegir entre iniciar sesión y registrar usuario
-opcion = st.sidebar.selectbox("Menú:", ["Inicio", "Iniciar sesión", "Registrarse", "Actualizar contraseña","Buscar recetas"])
+opcion = st.sidebar.selectbox("Menú:", ["Inicio", "Iniciar sesión",
+                    "Registrarse", "Actualizar contraseña","Buscar recetas"])
 
 # Función para buscar recetas por nombre y país
 def buscar_recetas(recetas, termino_busqueda):
+    """
+    Busca recetas por nombre o país en una lista de recetas.
+
+    Args: recetas (list): Una lista de recetas., termino_busqueda (str): El termino de búsqueda.
+
+    Returns: Una lista de recetas que coincidan con el termino de búsqueda.
+    """
     recetas_encontradas = []
     for receta in recetas:
-        # Convertimos los atributos de la receta a minúsculas para una búsqueda insensible a mayúsculas y minúsculas
+        # Convertimos los atributos de la receta a minúsculas para una búsqueda
+        # insensible a mayúsculas y minúsculas
         nombre = receta.nombre.lower()
         pais = receta.pais.lower()
-        # Verificamos si el término de búsqueda está en el nombre o país de la receta
+        # Verificamos si el término de búsqueda está en el nombre o país de 
+        # la receta
         if termino_busqueda.lower() in nombre or termino_busqueda.lower() in pais:
-            # Si encontramos una coincidencia, agregamos la receta a la lista de recetas encontradas
+            # Si encontramos una coincidencia, agregamos la receta a la lista
             recetas_encontradas.append(receta)
     return recetas_encontradas
 
 # Función principal
 def main():
+    """
+    Función principal del programa.
+
+    Args: None
+
+    Returns: None
+    """
 
     # Creación
     comidas = recetas.crear_recetas()
