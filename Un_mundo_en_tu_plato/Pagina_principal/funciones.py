de349a216a895e59bcb92f26b7ec0cb2742b0374
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2024-04-14 23:33:20 -0500

    Update funciones.py

--- Un_mundo_en_tu_plato/Pagina_principal/funciones.py
+++ Un_mundo_en_tu_plato/Pagina_principal/funciones.py
@@ -5,11 +5,23 @@ import numpy as np
 
 
 def mostrar_inicio():
+    """
+    Muestra el contenido de la página de inicio.
+
+    Args: None
+
+    Returns: None
+
+    """"
     st.write("Bienvenido al Inicio")
     st.write("Aquí encontrarás información sobre las funciones de la página:")
-    st.write("- Guardar Recetas: Permite a los usuarios guardar nuevas recetas en la base de datos.")
-    st.write("- Consultar Información de los Platos: Proporciona información detallada sobre los platos disponibles.")
-    st.write("- Ver Distribuciones Estadísticas: Muestra distribuciones estadísticas sobre los platos.")
+    st.write("- Guardar Recetas: Permite a los usuarios guardar nuevas recetas\
+    en la base de datos.")
+    st.write("- Consultar Información de los Platos: Proporciona información\
+    detallada sobre los platos disponibles.")
+    st.write("- Ver Distribuciones Estadísticas: Muestra distribuciones\
+    estadísticas sobre los platos.")
+
 
 """ def mostrar_grafico_popularidad(platos, popularidad):
     # Crear gráfico de barras
@@ -22,6 +34,7 @@ def mostrar_inicio():
 
     # Mostrar gráfico en Streamlit
     st.pyplot() """
+
 
 """def visualizar_distribucion_recetas(recetas):
     # Cargar datos geoespaciales de los países
@@ -40,12 +53,14 @@ def mostrar_inicio():
 
     # Mostrar el mapa interactivo
     fig, ax = plt.subplots(figsize=(10, 6))
-    world.plot(column='recetas', cmap='YlGn', linewidth=0.8, ax=ax, edgecolor='0.8', legend=True)
+    world.plot(column='recetas', cmap='YlGn', linewidth=0.8, ax=ax, 
+    edgecolor='0.8', legend=True)
     ax.set_title('Distribución Geográfica de Recetas')
     ax.set_axis_off()
 
     # Mostrar el mapa en Streamlit
     st.pyplot(fig) """
+
 
 # Ejemplo de uso:
 recetas = {
@@ -54,14 +69,18 @@ recetas = {
     "Pad Thai": "Tailandia",
     # Agregar más recetas con sus países de origen
 }
+
 # Ejemplo de uso:
 platos = ["Tacos al Pastor", "Pizza Margarita", "Pad Thai"]
 popularidad = [100, 75, 50]
 # Diccionario de recetas con ingredientes por porción
 recetas = {
-    "Tacos al Pastor": {"Carne de cerdo": 100, "Piña": 50, "Cebolla": 30, "Cilantro": 10, "Tortillas de maíz": 2},
-    "Pizza Margarita": {"Masa de pizza": 200, "Tomate": 100, "Mozzarella": 150, "Albahaca": 5},
-    "Pad Thai": {"Fideos de arroz": 100, "Tofu": 50, "Huevo": 30, "Brotes de soja": 20, "Cacahuetes": 10, "Salsa de tamarindo": 50}
+    "Tacos al Pastor": {"Carne de cerdo": 100, "Piña": 50, "Cebolla": 30,
+                        "Cilantro": 10, "Tortillas de maíz": 2},
+    "Pizza Margarita": {"Masa de pizza": 200, "Tomate": 100, "Mozzarella": 150,
+                        "Albahaca": 5},
+    "Pad Thai": {"Fideos de arroz": 100, "Tofu": 50, "Huevo": 30, "Brotes de\
+                       soja": 20, "Cacahuetes": 10, "Salsa de tamarindo": 50}
 }
 
 # Datos de ejemplo de recetas
