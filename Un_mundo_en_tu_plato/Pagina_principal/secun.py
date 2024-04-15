d26e59b517d14aac9f0e61a54be3b98075e4a998
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2024-04-14 22:14:31 -0500

    Update secun.py

--- Un_mundo_en_tu_plato/Pagina_principal/secun.py
+++ Un_mundo_en_tu_plato/Pagina_principal/secun.py
@@ -5,13 +5,15 @@ import funciones
 st.title("Contenido")
 
 # Menú de opciones desplegables
-opcion = st.sidebar.selectbox("Selecciona una opción:", ["Información", "Opción 2", "Opción 3"])
+opcion = st.sidebar.selectbox("Selecciona una opción:", ["Información", "Guardar Recetas", "Consultar Información de los Platos", "Distribuciones Estadísticas"])
 
 # Mostrar contenido según la opción seleccionada
 if opcion == "Información":
     st.write("Este es el contenido de la Opción 1.")
     funciones.mostrar_inicio()
-elif opcion == "Opción 2":
+elif opcion == "Guardar Recetas":
     st.write("Este es el contenido de la Opción 2.")
-elif opcion == "Opción 3":
+elif opcion == "Consultar Información de los Platos":
     st.write("Este es el contenido de la Opción 3.")
+elif opcion == "Distribuciones Estadísticas":
+    st.write("")
