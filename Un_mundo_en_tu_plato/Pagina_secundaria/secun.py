933e6e3d0fcc3d37f9e2f637fc98a7544f0d2052
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2024-04-14 19:55:28 -0500

    Update secun.py

--- Un_mundo_en_tu_plato/Pagina_secundaria/secun.py
+++ Un_mundo_en_tu_plato/Pagina_secundaria/secun.py
@@ -10,7 +10,7 @@ opcion = st.selectbox("Selecciona una opción:", ["Opción 1", "Opción 2", "Opc
 # Mostrar contenido según la opción seleccionada
 if opcion == "Opción 1":
     st.write("Este es el contenido de la Opción 1.")
-    graficos.mostrar_opcion_1()
+    #graficos.mostrar_opcion_1()
 elif opcion == "Opción 2":
     st.write("Este es el contenido de la Opción 2.")
 elif opcion == "Opción 3":
