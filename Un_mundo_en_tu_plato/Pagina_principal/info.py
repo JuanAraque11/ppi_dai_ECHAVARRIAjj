ce2fc1e0fa6c44212bb660bad38a003a4e5e41c2
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2024-04-25 13:58:37 -0500

    Update info.py

--- Un_mundo_en_tu_plato/Pagina_principal/info.py
+++ Un_mundo_en_tu_plato/Pagina_principal/info.py
@@ -48,6 +48,28 @@ def info2():
     
     # Título y autor
     st.title("Un mundo en tu plato")
+    st.write('''Emprende un viaje culinario alrededor del mundo sin salir de\
+    casa.
+    Descubre recetas auténticas de cada rincón del planeta, desde la vibrante\
+    cocina mexicana hasta los exóticos sabores tailandeses.
+
+    Explora una amplia variedad de platos, desde clásicos familiares hasta\
+    innovadoras creaciones de chefs galardonados.
+
+    Aprende a preparar platos deliciosos con instrucciones detalladas paso a\
+    paso y videos explicativos.
+
+    Conoce los ingredientes de cada receta, su origen y sus propiedades\
+    nutricionales.
+
+    Personaliza tu experiencia con filtros avanzados que te permiten buscar\
+    recetas por región, tipo de cocina, ingredientes y preferencias dietéticas.
+
+    Guarda tus recetas favoritas para acceder a ellas fácilmente más tarde.
+
+    Comparte tus descubrimientos culinarios con amigos y familiares en las\
+    redes sociales.
+    ''')
 
 
 def mostrar_ventana_emergente():
