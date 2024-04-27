c37566a4ad26f94994ea5066ea3d3984cdf6a904
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2024-04-26 19:39:47 -0500

    Update principal.py

--- Un_mundo_en_tu_plato/Pagina_principal/principal.py
+++ Un_mundo_en_tu_plato/Pagina_principal/principal.py
@@ -4,7 +4,7 @@ import login2
 import secun
 import recetas
 import register
-import streamlit_authenticator as stauth
+# import streamlit_authenticator as stauth
 
 # Menú desplegable para elegir entre iniciar sesión y registrar usuario
 opcion = st.sidebar.selectbox("Menú:", ["Inicio", "Registrarse", "Actualizar contraseña", "Buscar recetas", "Receta al azar", "Buscar por valoración","Información y contacto"])
