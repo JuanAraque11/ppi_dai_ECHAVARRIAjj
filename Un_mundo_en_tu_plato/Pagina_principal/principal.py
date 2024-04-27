a82619da006611d05ea6fa844968db6a7d0407dd
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2024-04-26 19:34:58 -0500

    login

--- Un_mundo_en_tu_plato/Pagina_principal/principal.py
+++ Un_mundo_en_tu_plato/Pagina_principal/principal.py
@@ -1,38 +1,13 @@
 import streamlit as st
 import info
-import login
+import login2
+import secun
 import recetas
 import register
-from usuario import Usuario
+import streamlit_authenticator as stauth
 
 # Menú desplegable para elegir entre iniciar sesión y registrar usuario
 opcion = st.sidebar.selectbox("Menú:", ["Inicio", "Registrarse", "Actualizar contraseña", "Buscar recetas", "Receta al azar", "Buscar por valoración","Información y contacto"])
-
-# Función para buscar recetas por nombre y país
-def buscar_recetas(recetas, termino_busqueda):
-    """
-    Busca recetas por nombre o país en una lista de recetas.
-
-    Args: recetas (list): Una lista de recetas., termino_busqueda (str): El termino de búsqueda.
-
-    Returns: Una lista de recetas que coincidan con el termino de búsqueda.
-    """
-    recetas_encontradas = []
-    for receta in recetas:
-
-        # Convertimos los atributos de la receta a minúsculas para una búsqueda
-        # insensible a mayúsculas y minúsculas
-        nombre = receta.nombre.lower()
-        pais = receta.pais.lower()
-
-        # Verificamos si el término de búsqueda está en el nombre o país de 
-        # la receta
-        if termino_busqueda.lower() in nombre or termino_busqueda.lower() in pais:
-            
-            # Si encontramos una coincidencia, agregamos la receta a la lista
-            recetas_encontradas.append(receta)
-    return recetas_encontradas
-
 
 # Función principal
 def main():
@@ -49,15 +24,32 @@ def main():
     # if estado_politi:
 
     if opcion == "Información y contacto":
-
         info.info1()
+
     elif opcion == "Registrarse":
         register.register_user()
         # if st.button("Ver lista de usuarios"):
             # register.show_registered_users()
+
     elif opcion == "Inicio":
 
         info.info2()
+
+        authenticator = stauth.Authenticate(login2.names, login2.usernames, login2.passwords,
+        "sales_dashboard", "abcdef", cookie_expiry_days=30)
+
+        name, authentication_status, username = authenticator.login("Login", "main")
+
+        if authentication_status == False:
+            st.error("Username/password is incorrect")
+
+        if authentication_status == None:
+            st.warning("Please enter your username and password")
+
+        if authentication_status:
+            secun.mostrar_pagina2()
+
+        """
         estado, en_sesion = login.login_user()
         if not estado:
             st.info("Por favor, inicie sesión.")
@@ -66,6 +58,7 @@ def main():
             # Redirigir al usuario a un enlace externo después de iniciar sesión
             st.markdown("[Ir al enlace](https://unmundoentuplato-funciones.streamlit.app/)")
             st.write("La sesion pertenece a ", en_sesion.get_username())
+        """
     elif opcion == "Actualizar contraseña":
 
         register.change_password()
--- Un_mundo_en_tu_plato/Pagina_principal/secun.py
+++ Un_mundo_en_tu_plato/Pagina_principal/secun.py
@@ -4,72 +4,72 @@ import pandas as pd
 #import scipy.stats as stats
 #import matplotlib.pyplot as plt
 
-
-# Título de la página
-st.title("Contenido")
-
-# Menú de opciones desplegables
-opcion = st.sidebar.selectbox("Selecciona una opción:", ["Información", "Guardar Recetas", "Consultar Información de los Platos", "Estadísticas de recetas", "Carga tus recetas", "Popularidad", "Distribuciones"])
-
-# Mostrar contenido según la opción seleccionada
-if opcion == "Información":
-    funciones.mostrar_inicio()
-elif opcion == "Guardar Recetas":
-    st.write("En proceso...")
-elif opcion == "Consultar Información de los Platos":
+def mostrar_pagina2():
     # Título de la página
-    st.title("Calculadora de Ingredientes")
+    st.title("Contenido")
 
-    # Solicitar información al usuario
-    plato = st.selectbox("Selecciona un plato:", list(funciones.recetas.keys()))
-    num_personas = st.number_input("Número de personas que van a comer:", min_value=1, value=1)
+    # Menú de opciones desplegables
+    opcion = st.sidebar.selectbox("Selecciona una opción:", ["Información", "Guardar Recetas", "Consultar Información de los Platos", "Estadísticas de recetas", "Carga tus recetas", "Popularidad", "Distribuciones"])
 
-    # Calcular ingredientes y mostrar resultado
-    if st.button("Calcular"):
-        cantidad_ingredientes = funciones.calcular_ingredientes(plato, num_personas)
-        st.write("Cantidad de Ingredientes Necesarios:")
-        for ingrediente, cantidad in cantidad_ingredientes.items():
-            st.write(f"- {ingrediente}: {cantidad} gramos")
-elif opcion == "Estadísticas de recetas":
-    st.write("")
-    # Crear DataFrame de Pandas con los datos de las recetas
-    """df_recetas = pd.DataFrame(funciones.datos_recetas)
+    # Mostrar contenido según la opción seleccionada
+    if opcion == "Información":
+        funciones.mostrar_inicio()
+    elif opcion == "Guardar Recetas":
+        st.write("En proceso...")
+    elif opcion == "Consultar Información de los Platos":
+        # Título de la página
+        st.title("Calculadora de Ingredientes")
 
-    # Calcular estadísticas descriptivas
-    descripcion_calorias = stats.describe(df_recetas["Calorías"])
-    descripcion_grasas = stats.describe(df_recetas["Grasas (g)"])
-    descripcion_proteinas = stats.describe(df_recetas["Proteínas (g)"])
+        # Solicitar información al usuario
+        plato = st.selectbox("Selecciona un plato:", list(funciones.recetas.keys()))
+        num_personas = st.number_input("Número de personas que van a comer:", min_value=1, value=1)
 
-    # Mostrar estadísticas descriptivas
-    st.title("Análisis de Ingredientes Nutricionales")
-    st.write("Estadísticas de Calorías:")
-    st.write(descripcion_calorias)
-    st.write("Estadísticas de Grasas:")
-    st.write(descripcion_grasas)
-    st.write("Estadísticas de Proteínas:")
-    st.write(descripcion_proteinas) """
+        # Calcular ingredientes y mostrar resultado
+        if st.button("Calcular"):
+            cantidad_ingredientes = funciones.calcular_ingredientes(plato, num_personas)
+            st.write("Cantidad de Ingredientes Necesarios:")
+            for ingrediente, cantidad in cantidad_ingredientes.items():
+                st.write(f"- {ingrediente}: {cantidad} gramos")
+    elif opcion == "Estadísticas de recetas":
+        st.write("")
+        # Crear DataFrame de Pandas con los datos de las recetas
+        """df_recetas = pd.DataFrame(funciones.datos_recetas)
 
-elif opcion == "Carga tus recetas":
-    
-    # Widget para cargar archivo CSV o Excel
-    archivo = st.file_uploader("Cargar archivo con tus recetas:", type=["csv", "xlsx"])
+        # Calcular estadísticas descriptivas
+        descripcion_calorias = stats.describe(df_recetas["Calorías"])
+        descripcion_grasas = stats.describe(df_recetas["Grasas (g)"])
+        descripcion_proteinas = stats.describe(df_recetas["Proteínas (g)"])
 
-    # Si se carga un archivo, leer los datos y mostrarlos
-    if archivo is not None:
-        # Leer los datos del archivo
-        if archivo.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":  # Excel
-            df_datos_adicionales = pd.read_excel(archivo)
-        else:  # CSV
-            df_datos_adicionales = pd.read_csv(archivo)
+        # Mostrar estadísticas descriptivas
+        st.title("Análisis de Ingredientes Nutricionales")
+        st.write("Estadísticas de Calorías:")
+        st.write(descripcion_calorias)
+        st.write("Estadísticas de Grasas:")
+        st.write(descripcion_grasas)
+        st.write("Estadísticas de Proteínas:")
+        st.write(descripcion_proteinas) """
+
+    elif opcion == "Carga tus recetas":
         
-        # Mostrar los datos cargados
-        st.write("Datos adicionales cargados:")
-        st.write(df_datos_adicionales)
+        # Widget para cargar archivo CSV o Excel
+        archivo = st.file_uploader("Cargar archivo con tus recetas:", type=["csv", "xlsx"])
 
-elif opcion == "Popularidad":
-    st.write("")
-    #funciones.mostrar_grafico_popularidad(funciones.platos, funciones.popularidad)
+        # Si se carga un archivo, leer los datos y mostrarlos
+        if archivo is not None:
+            # Leer los datos del archivo
+            if archivo.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":  # Excel
+                df_datos_adicionales = pd.read_excel(archivo)
+            else:  # CSV
+                df_datos_adicionales = pd.read_csv(archivo)
+            
+            # Mostrar los datos cargados
+            st.write("Datos adicionales cargados:")
+            st.write(df_datos_adicionales)
 
-elif opcion == "Distribuciones":
-    st.write("")
-    # funciones.visualizar_distribucion_recetas(funciones.recetas)
+    elif opcion == "Popularidad":
+        st.write("")
+        #funciones.mostrar_grafico_popularidad(funciones.platos, funciones.popularidad)
+
+    elif opcion == "Distribuciones":
+        st.write("")
+        # funciones.visualizar_distribucion_recetas(funciones.recetas)
