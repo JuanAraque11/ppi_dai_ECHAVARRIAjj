b8ab467ed6f482f064a7f366403db8259a66852c
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2024-04-28 21:09:59 -0500

    Update recetas.py

--- Un_mundo_en_tu_plato/Pagina_principal/recetas.py
+++ Un_mundo_en_tu_plato/Pagina_principal/recetas.py
@@ -8,7 +8,13 @@ ruta = "https://raw.githubusercontent.com/JuanAraque11/ppi_dai_ECHAVARRIAjj/main
 data = pd.read_csv(ruta, delimiter="|")
 
 def mostrar_receta_aleatoria():
-    # Seleccionar una fila aleatoria del DataFrame
+    """
+    Muestra una receta al azar de la base de datos.
+
+    Args: None
+
+    Returns: None
+    """
     aleatoria = st.button("Buscar una receta al azar")
 
     if aleatoria:
@@ -22,15 +28,27 @@ def mostrar_receta_aleatoria():
 
 
 def reemplazar_nulos():
-    # Reemplazar los nulos de la columna 'Ingredientes' con una cadena vacía
+    """
+    Reemplaza los valores nulos de la base de datos.
+
+    Args: None
+
+    Returns: None 
+    """
     data['Ingredientes'] = data['Ingredientes'].fillna('')
     data['Valoracion'] = data['Valoracion'].fillna(0)
     data['Dificultad'] = data['Dificultad'].fillna('media')
     data['Tipo'] = data['Tipo'].fillna('Entrante')
 
 
-def buscar_receta_por_ingrediente(ingrediente):    
+def buscar_receta_por_ingrediente(ingrediente):  
+    """
+    Busca recetas por el ingrediente especificado.
+
+    Args: ingrediente (str): El ingrediente a buscar.  
     
+    Returns: None
+    """
     reemplazar_nulos()
 
     recetas_filtradas = data[data['Ingredientes'].str.contains(ingrediente)]
@@ -56,13 +74,15 @@ def buscar_por_valoracion(valoracion, opcion):
     if opcion == 'mayores':
         recetas_filtradas = data[data['Valoracion'] > valoracion]
     elif opcion == 'menores':
-        recetas_filtradas = data[(data['Valoracion'] < valoracion) & (data['Valoracion'] != 0)]
+        recetas_filtradas = data[(data['Valoracion'] < valoracion) & 
+         (data['Valoracion'] != 0)]
     elif opcion == 'iguales':
         recetas_filtradas = data[data['Valoracion'] == valoracion]
     elif opcion == 'sin valoración':
         recetas_filtradas = data[data['Valoracion'] == 0]
     else:
-        st.error("Opción de búsqueda no válida. Las opciones válidas son 'mayores', 'menores' o 'iguales'.")
+        st.error("Opción de búsqueda no válida. Las opciones válidas son\
+                  'mayores', 'menores' o 'iguales'.")
         return None
     
     return recetas_filtradas
