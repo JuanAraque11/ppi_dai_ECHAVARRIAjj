bff966c9dd18ca482661da1d445ba7970610923a
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2024-04-28 21:03:01 -0500

    Update principal.py

--- Un_mundo_en_tu_plato/Pagina_principal/principal.py
+++ Un_mundo_en_tu_plato/Pagina_principal/principal.py
@@ -22,7 +22,12 @@ def main():
 
     if aceptado:
         # Menú desplegable para elegir entre iniciar sesión y registrar usuario
-        opcion = st.sidebar.selectbox("Menú:", ["Inicio", "Registrarse", "Actualizar contraseña", "Buscar recetas", "Receta al azar", "Buscar por valoración","Información y contacto"])
+        opcion = st.sidebar.selectbox("Menú:", ["Inicio", "Registrarse", 
+                                                "Actualizar contraseña", 
+                                                "Buscar recetas", 
+                                                "Receta al azar", 
+                                                "Buscar por valoración",
+                                                "Información y contacto"])
 
 
         # estado_politi = info.mostrar_ventana_emergente()
@@ -46,7 +51,7 @@ def main():
                 st.info("Por favor, inicie sesión.")
             else:
                 
-                # Redirigir al usuario a un enlace externo después de iniciar sesión
+                # Redirigir al usuario a un enlace después de iniciar sesión
                 st.markdown("[Ir al enlace](https://unmundoentuplato-funciones.streamlit.app/)")
 
 
@@ -59,12 +64,15 @@ def main():
             ingrediente_busqueda = st.text_input("Ingrese un ingrediente:")
             buscar_button = st.button("Buscar recetas")
             if buscar_button:
-                recetas_encontradas = recetas.buscar_receta_por_ingrediente(ingrediente_busqueda)
+                recetas_encontradas = recetas.buscar_receta_por_ingrediente
+                 (ingrediente_busqueda)
                 if recetas_encontradas.empty:
                     st.write("No se encontraron recetas con ese ingrediente.")
                 else:
                     st.write("Recetas encontradas:")
-                    st.write(recetas_encontradas[['Nombre', 'Tiempo', 'Ingredientes', 'Link_receta']])
+                    st.write(recetas_encontradas[['Nombre', 'Tiempo',
+                                                  'Ingredientes', 
+                                                  'Link_receta']])
 
         # else:
             # st.stop()
@@ -75,19 +83,27 @@ def main():
         elif opcion == "Buscar por valoración":
 
             st.title("Buscar por valoración")
-            valoracion = st.number_input("Ingrese la valoración a buscar", min_value=1.0, max_value=5.0, step=0.1, value=3.0)
-            opcion_buscar = st.radio("Selecciona una opción:", ['mayores', 'menores', 'iguales', 'sin valoración'])
+            valoracion = st.number_input("Ingrese la valoración a buscar",
+                                         min_value=1.0, max_value=5.0,
+                                         step=0.1, value=3.0)
+            opcion_buscar = st.radio("Selecciona una opción:",
+                                     ['mayores', 'menores', 'iguales',
+                                      'sin valoración'])
 
             if st.button("Buscar"):
-                recetas_valoradas = recetas.buscar_por_valoracion(valoracion, opcion_buscar)
+                recetas_valoradas = recetas.buscar_por_valoracion(valoracion,
+                                                                  opcion_buscar)
                 if recetas_valoradas.empty:
                     st.write("No se encontraron recetas.")
                 else:
                     st.write("Recetas encontradas:")
-                    st.write(recetas_valoradas[['Nombre', 'Tiempo', 'Ingredientes', 'Valoracion','Link_receta']])
+                    st.write(recetas_valoradas[['Nombre', 'Tiempo',
+                                                'Ingredientes', 'Valoracion',
+                                                'Link_receta']])
     else:
-        # Mostrar mensaje indicando que el usuario debe aceptar los términos y condiciones
-        st.warning("Debe aceptar los Términos y Condiciones para utilizar la aplicación.")
+        # Mostrar mensaje para aceptar los términos y condiciones
+        st.warning("Debe aceptar los Términos y Condiciones para utilizar la\
+                    aplicación.")
 
 
 
