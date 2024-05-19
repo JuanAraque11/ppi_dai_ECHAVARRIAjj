07cecb12ecec44e77de5e4e488563601a687eead
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2024-05-19 12:55:35 -0500

    Update restaurantes.py

--- Un_mundo_en_tu_plato/Pagina_principal/restaurantes.py
+++ Un_mundo_en_tu_plato/Pagina_principal/restaurantes.py
@@ -29,8 +29,7 @@ def elegir_restaurantes():
 
     st.title("Elegir ciudad para ver restaurantes.")
     seleccion_ciudad = st.selectbox("Selecciona la ciudad:", ['New Delhi','Gurgaon', 'Noida', 'Faridabad', 'Ghaziabad '])
-    st.write(datos.head(2))
-    """
+    
     if st.button("Buscar"):
         hallar_restaurantes = datos[datos['City'] == seleccion_ciudad]
         if hallar_restaurantes.empty:
@@ -42,4 +41,4 @@ def elegir_restaurantes():
             plt.xlabel('City')
             plt.ylabel('Frecuencia')
             plt.xticks(rotation=45)  # Rotar las etiquetas del eje x si son muchas o muy largas
-            st.pyplot(plt)"""
+            st.pyplot(plt)
