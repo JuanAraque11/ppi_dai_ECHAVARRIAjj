755121d9c37350ecd2c72b23e075efcfe8e922ba
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2024-05-19 11:47:38 -0500

    Update restaurantes.py

--- Un_mundo_en_tu_plato/Pagina_principal/restaurantes.py
+++ Un_mundo_en_tu_plato/Pagina_principal/restaurantes.py
@@ -4,6 +4,15 @@ from scipy.spatial.distance import cdist
 
 import geopandas as gpd
 
-url = ""
+url = "https://raw.githubusercontent.com/JuanAraque11/ppi_dai_ECHAVARRIAjj/\
+main/Un_mundo_en_tu_plato/Datos/restaurantes.csv"
 
+# Cargar el dataset a partir de la ruta establecida
 datos = gpd.read_file(url)
+
+# Convertir las columnas de latitud y longitud a tipo numérico
+datos['Latitude'] = pd.to_numeric(datos['Latitude'])
+datos['Longitude'] = pd.to_numeric(datos['Longitude'])
+datos['Price range'] = pd.to_numeric(datos['Price range'])
+datos['Aggregate rating'] = pd.to_numeric(datos['Aggregate rating'])
+datos['Votes'] = pd.to_numeric(datos['Votes'])
