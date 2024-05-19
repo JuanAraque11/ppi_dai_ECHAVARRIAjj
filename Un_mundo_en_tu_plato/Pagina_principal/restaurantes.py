2517aaa5faec85f49c2ca1aba7a694efbf24ea83
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2024-05-19 14:51:47 -0500

    Update restaurantes.py

--- Un_mundo_en_tu_plato/Pagina_principal/restaurantes.py
+++ Un_mundo_en_tu_plato/Pagina_principal/restaurantes.py
@@ -22,8 +22,7 @@ datos['Address'] = datos['Address'].astype('string')
 # Eliminar las columnas de latitud y longitud con valores nulos
 datos = datos[(datos['Longitude'] != 0) | (datos['Latitude'] != 0)]
 
-
-NewDelhi = datos[datos['City'] == 'New Delhi']
+NewDelhi = datos[(datos['City'] == 'New Delhi') & (datos['Longitude'] != 34.0)]
 Gurgaon = datos[datos['City'] == 'Gurgaon']
 Noida = datos[datos['City'] == 'Noida']
 Faridabad = datos[datos['City'] == 'Faridabad']
